a2c44c5cca043a6af43d540c8c7cac73e2dcaa6f
hskim <75778561+kingmuraa@users.noreply.github.com>
2023-08-14 12:50:54 +0900

    wow

--- react-app/src/components/Footer.jsx
+++ react-app/src/components/Footer.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 
 const Footer = () => {
-    return <div>
-        Footer
-    </div>;
-    
-};
+    return (
+    <>
+        <div className="container mx-auto px-5 flex justify-between py-4 items-center">
+            Footer
+        </div>      
+    </>
+)};
 
 export default Footer;
