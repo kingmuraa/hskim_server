7492c8852df4c505de513431420446e7fff26c20
hskim <75778561+kingmuraa@users.noreply.github.com>
2023-08-21 09:40:52 +0900

    [AWS 접속 테스트] margin top

--- react-app/src/components/Footer.jsx
+++ react-app/src/components/Footer.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 const Footer = () => {
     return (
-    <section className="bg-dark-hard h-96">
+    <section className="bg-dark-hard h-96 mt-5">
         <div className="container mx-auto px-5 flex justify-between py-4 items-center">
             <p className="text-[#959EAD] font-semibold">Footer</p>
         </div>      
